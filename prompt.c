41810393e746420cca20bb7c496f27d6c51d6d4f
Ya Zhuang <zhuangya@calorietech.com>
2015-11-03 14:30:59 +0800

    change: ifdef for prompt.c

--- prompt.c
+++ prompt.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <editline/readline.h>
-/* #include <editline/history.h> */
+/* If we are compiling on Windows compile there functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline (char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer) + 1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy) - 1] = '\0';
+  return cpy;
+}
+
+/* fake add_history function */
+void add_history(char* unused) {}
+
+#else
+  #include <editline/readline.h>
+  #ifdef __linux__
+    #include <editline/history.h>
+  #endif
+#endif
 
 int main (int argc, char** argv) {
 
   /* Print Version and Exit Information */
-  puts("Lispy Version 0.0.0.0.1");
+  puts("YaLang Version 0.0.0.0.1");
+  puts("There is no spoon");
   puts("Press Ctrl + c to Exit\n");
 
   /* In a never ending loop */
@@ -15,13 +39,13 @@ int main (int argc, char** argv) {
 
 
     /* Output our prompt and get input */
-    char* input = readline("lispy> ");
+    char* input = readline("YaLang> ");
 
     /* Add input to history */
     add_history(input);
 
     /* Echo input back to user */
-    printf("No you're a %s\n", input);
+    printf("May the force be with you: %s\n", input);
 
     /* Free retrieved input */
     free(input);
