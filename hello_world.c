d140f9e256ba46e4ddc8054f7802f8b79423661d
Ya Zhuang <zhuangya@calorietech.com>
2015-11-03 14:09:02 +0800

    add: for/while loop practice

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+int forloop (int);
+int whileloop (int);
+
 int main (int argc, char** argv) {
-  puts("Hello, world!");
+  //forloop(5);
+  whileloop(5);
+  return 0;
+}
+
+int forloop (int times) {
+  for (int i = 0; i < times; i++) {
+    puts("Hello, world!");
+  }
+  return 0;
+}
+
+int whileloop (int times) {
+  while (times-- > 0) {
+    puts("Hello, world!");
+  }
   return 0;
 }
