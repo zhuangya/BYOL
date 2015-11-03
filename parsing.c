bcff96ecdd1b7e9b62ee6c8a0d5fa2f28d36975a
Ya Zhuang <zhuangya@calorietech.com>
2015-11-03 15:24:39 +0800

    add: mpc support grammer parser
    known issue:
    
    ^D would lead to segmentation fault 11

--- parsing.c
+++ parsing.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "deps/mpc/mpc.h"
 
 /* If we are compiling on Windows compile there functions */
 #ifdef _WIN32
@@ -29,6 +30,26 @@ void add_history(char* unused) {}
 
 int main (int argc, char** argv) {
 
+
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr = mpc_new("expr");
+  mpc_parser_t* Lispy = mpc_new("lispy");
+  mpc_parser_t* Abba = mpc_new("abba");
+  mpc_parser_t* Abab = mpc_new("abab");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+    "\
+      abba: /[ab]+/; \
+      abab: /(?:ab)a?/; \
+      number: /-?[0-9]+/ ; \
+      operator: '+' | '-' | '*' | '/'; \
+      expr: <number> | '(' <operator> <expr>+ ')' ; \
+      lispy: /^/ <abba> <operator> <expr>+ /$/ ; \
+    ",
+    Abba, Abab, Number, Operator, Expr, Lispy);
+
+
   /* Print Version and Exit Information */
   puts("YaLang Version 0.0.0.0.1");
   puts("There is no spoon");
@@ -44,11 +65,21 @@ int main (int argc, char** argv) {
     /* Add input to history */
     add_history(input);
 
-    /* Echo input back to user */
-    printf("May the force be with you: %s\n", input);
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+      /* On Success Print the AST */
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      /* Otherwise Print the Error */
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
 
     /* Free retrieved input */
     free(input);
   }
+
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
   return 0;
 }
